eb0078fcaa6c2a08c527aba3d0ca540bc1fc2a9a
YaswanthShanmukha <74482877+YaswanthShanmukha@users.noreply.github.com>
2021-02-28 14:16:09 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+console.log(typeof "Yaswanth shanmukha");
+console.log(typeof "140");
+console.log(typeof 443);
+var ob
+console.log(typeof ob);
+var student = {
+    "name":"Yaswanth shanmukha",
+    "age" : 13
+};
+console.log(typeof student);
+
+var a = [10,20,30,40];
+console.log(a);
+console.log(a.length)
+console.log(a[3]);
+a.push(66)
+a.push("apple")
+console.log(a);
+console.log(a.length);
+a.pop();
+console.log(a);
+var b = [[10,20],[30,40],[50,60]];
+console.log(b);
+console.log(b[2][0]);
+b.push([1,2]);
+console.log(b);
